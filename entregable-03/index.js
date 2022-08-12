f7ffa11eef432fef3a3bfec49c92e54d7ea4708e
elicassini <elielicassini@gmail.com>
2022-08-11 23:59:27 -0300

    entrega 03

---
+++ entregable-03/index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+const Contenedor = require('../enentregable-2')
+
+const productos  = new Contenedor('../productos.txt');
+
+const PORT = 8080
+
+app.get('/productos', async (req, res) => {
+    return res.send(`${ JSON.stringify(await productos.getAll())}`)
+})
+
+app.get('/productoRandom', async (req, res) => {
+    let p = await productos.getAll()    
+    return res.send(`${  JSON.stringify(p[Math.floor((Math.random() * (p.length-0)) +0)])}`)
+})
+
+const server = app.listen (PORT, () => {
+    console.log(`servidor HTTP con express corriendo en el puerto ${PORT}`)
+})
+
+server.on('error', error => console.log(`error en servidor: ${error}`))
