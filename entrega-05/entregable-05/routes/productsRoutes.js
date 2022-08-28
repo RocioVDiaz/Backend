97ae0dc4339adf8359f9aee41c8a9f421bdb0dbc
elicassini <elielicassini@gmail.com>
2022-08-28 17:08:09 -0300

    cambios

--- entrega-05/entregable-05/routes/productsRoutes.js
+++ entrega-05/entregable-05/routes/productsRoutes.js
@@ -1,15 +1,33 @@
 const express = require('express')
 const router = express.Router();
 
-router.get('/mostrarProducto', (req, res)=>{
-    res.render('productos', {lista:'productos'})
+let id = 3; 
+let listaProductos =[
+    {title:"Calculadora", price:234.56, thumbnail:"https://cdn3.iconfinder.com/data/icons/education-209/64/calculator-math-tool-school-256.png",id:1},
+    {title:"Globo Terráqueo", price:345.67, thumbnail:"https://cdn3.iconfinder.com/data/icons/education-209/64/globe-earth-geograhy-planet-school-256.png",id:2},
+    {title:"Escuadra", price:123.45, thumbnail:"https://cdn3.iconfinder.com/data/icons/education-209/64/ruler-triangle-stationary-school-256.png", id:3}
+]
+router.get('/mostrarProducto', async (req, res)=>{
+    res.render('productos', {productos: listaProductos})
 
 })
+
 router.get('/agregarProducto', (req, res)=>{
+    console.log("hola")
     res.render('agregarProducto')
 })
+router.get ('detalle/:id', (req,res)=>{
+   let id = req.params.id;
+   let miProducto = listaProductos.filter(p=>p.id == id);
+   res.send(miProducto) 
+})
+
 router.post('/',(req, res)=>{
     let datos= req.body;
-    res.redirect('/producto/agregarProducto');
+    datos.id=id++;
+    listaProductos=[...listaProductos,datos]
+    res.redirect('/productos/agregarProducto');
+    
+
 })
 module.exports = router;
